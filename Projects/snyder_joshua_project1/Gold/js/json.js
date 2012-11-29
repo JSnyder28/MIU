839ddc442a6663100d14efe5573e327006bcbadd
Joshua Snyder <jsnyder28@fullsail.edu>
2012-11-28 18:59:53 -0600

    Initial commit from master branch.

---
+++ Projects/snyder_joshua_project1/Gold/js/json.js
@@ -0,0 +1,200 @@
+var json = {
+	"Recipe1": {
+		"cat": ["Category:", "American"],
+		"date": ["Date Added:", "2012-11-13"],
+		"rcpName": ["Recipe Name:", "Fiery Tex-Mex Chipotle Cheeseburgers"],
+		"directions": ["Directions:", "1 1/2 pounds lean ground beef, 2 tablespoons Tex-Mex chili seasoning, 2 teaspoons chipotles in adobo sauce, 1/4 cup beer of choice, Salt and pepper, 2 slices pepper jack cheese, Hambruger buns, toasted, Lettuce, tomatoes, onions, Sliced avocado is optional. Directions: In a mixing bowl, stir to combine ground beef, chili seasoning, 2 teaspoons chopped chipotles and beer. Wet your hands to prevent sticking and shape into 4 patties slightly larger than the buns; set aside covered in refrigerator if not cooking immediately. Set up grill for direct cooking over high heat. Brush and oil grill before cooking. Season burgers with salt and pepper and place on hot oiled grill. Cook 4 to 5 minutes per side for medium. Place cheese slices on burgers 2 to 3 minutes before burgers are done. Serve hot on toasted bun with lettuce, tomato, onion, sliced avocado, if desired, and a dollop of chipotle mayonnaise. INDOOR: Follow instructions for preparing burgers. Preheat broiler. Place burgers on wire rack over foil-lined baking sheet or broiler pan. Broil 6 inches from heat source for 4 to 5 minutes per side for medium. Place cheese slices on burger 1 to 2 minutes before done. Serve hot on toasted bun with lettuce, tomato, onion, sliced avocado (if desired), and a dollop of chipotle mayonnaise."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe2": {
+		"cat": ["Category:", "American"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Chicken Strips"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe3": {
+		"cat": ["Category:", "American"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Steak"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe4": {
+		"cat": ["Category:", "American"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Chili Dog"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	},
+
+	"Recipe5": {
+		"cat": ["Category:", "Chinese"],
+		"date": ["Date Added:", "2012-10-13"],
+		"rcpName": ["Recipe Name:", "Broccoli Beef"],
+		"directions": ["Directions:", "Stirfry the vegetables and add seared beef."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe6": {
+		"cat": ["Category:", "Chinese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "General Tso's Chicken"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe7": {
+		"cat": ["Category:", "Chinese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe8": {
+		"cat": ["Category:", "Chinese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	},
+
+	"Recipe9": {
+		"cat": ["Category:", "Italian"],
+		"date": ["Date Added:", "2012-11-10"],
+		"rcpName": ["Recipe Name:", "Meatball Sub"],
+		"directions": ["Directions:", "Make meatballs, add sauce, and put on bread of choice."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe10": {
+		"cat": ["Category:", "Italian"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe11": {
+		"cat": ["Category:", "Italian"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe12": {
+		"cat": ["Category:", "Italian"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	},
+
+	"Recipe13": {
+		"cat": ["Category:", "Japanese"],
+		"date": ["Date Added:", "2012-12-17"],
+		"rcpName": ["Recipe Name:", "Sushi"],
+		"directions": ["Directions:", "Roll desired fish in Salmon skin."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe14": {
+		"cat": ["Category:", "Japanese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Lettuce Wraps"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe15": {
+		"cat": ["Category:", "Japanese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe16": {
+		"cat": ["Category:", "Japanese"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	},
+
+	"Recipe17": {
+		"cat": ["Category:", "Mexican"],
+		"date": ["Date Added:", "2012-11-5"],
+		"rcpName": ["Recipe Name:", "Enchiladas"],
+		"directions": ["Directions:", "Bake in the oven for 35 minutes on 375."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe18": {
+		"cat": ["Category:", "Mexican"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe19": {
+		"cat": ["Category:", "Mexican"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe20": {
+		"cat": ["Category:", "Mexican"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	},
+
+	"Recipe21": {
+		"cat": ["Category:", "Seasonal"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe22": {
+		"cat": ["Category:", "Seasonal"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe23": {
+		"cat": ["Category:", "Seasonal"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe24": {
+		"cat": ["Category:", "Seasonal"],
+		"date": ["Date Added:", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	}
+
+};
